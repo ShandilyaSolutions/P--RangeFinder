3fff97e69668830606c9f009cdb58266b2b8405d
Amritanshu Shandilya <sandyamritanshu9715@gmail.com>
2022-10-18 22:11:05 +0530

    Added the basic vector display

---
+++ cppCodes/vectorOperation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x,y,z;
+    cout<<"Enter your vector coefficients : \n(xi^+yj^+zk^ format)"<<endl;
+    cout<<"Enter value of x : ";
+    cin>>x;
+    cout<<"Enter value of y : ";
+    cin>>y;
+    cout<<"Enter value of z : ";
+    cin>>z;
+    cout<<"Vector equation is : "<<endl;
+    cout<<"("<<x<<")i^ + ("<<y<<")j^ + ("<<z<<")k^"<<endl;
+    return 0;
+}
