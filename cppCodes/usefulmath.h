68970443307272005c2daa736c20767a3bf0e85a
Amritanshu Shandilya <sandyamritanshu9715@gmail.com>
2022-10-21 00:14:37 +0530

    Updated the library

--- cppCodes/usefulmath.h
+++ cppCodes/usefulmath.h
@@ -4,16 +4,26 @@
 
 float distance2d (float x1, float y1, float x2, float y2)
 {
-    /*Square root for 2d cordinate system*/
+    /*Distance formula for 2d cordinate system*/
     float dist;
-    dist = (x2-x1)+(y2-y1);
+    dist = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
     return (sqrt(dist));
 }
 
 
 float distance3d(float x1, float y1, float z1, float x2, float y2, float z2)
 {
+    //Distance formula for 3d cordinate system
     float dist;
-    dist = (x2-x1)+(y2-y1)+(z2-z1);
+    dist = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1));
     return (sqrtf(dist));
+}
+
+
+float distanceOrigin(float x, float y, float z)
+{
+    //Distance of a point from Origin of cordinate system
+    float dist;
+    dist = (x*x)+(y*y)+(z*z);
+    return dist;
 }
